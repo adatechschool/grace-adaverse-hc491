c7fd65fe89f98debf74c3eb6435ef19208df47cb
Chau <hoaichau20@hotmail.com>
2026-04-07 10:47:52 +0200

    création tables

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,8 +1,45 @@
-import { integer, pgTable, varchar } from "drizzle-orm/pg-core";
+import { integer, pgTable, serial, text, timestamp } from "drizzle-orm/pg-core";
+// import { defineRelations } from "drizzle-orm";
 
-export const usersTable = pgTable("users", {
-  id: integer().primaryKey().generatedAlwaysAsIdentity(),
-  name: varchar({ length: 255 }).notNull(),
-  age: integer().notNull(),
-  email: varchar({ length: 255 }).notNull().unique(),
+export const programmesTable = pgTable("programmes", {
+  id: serial("id").primaryKey(),
+  name: text("name").notNull(),
 });
+
+export const promotionsTable = pgTable("promotions", {
+  id: serial("id").primaryKey(),
+  name: text("name").notNull(),
+  date: timestamp("date").notNull(),
+});
+
+export const projectsTable = pgTable("projects", {
+  id: integer().primaryKey().generatedAlwaysAsIdentity(),
+  title: text("title").notNull(),
+  thumbnail: text("thumbnail"),
+  gitHubLink: text("gitHub_link").notNull(),
+  demoLink: text("demo_link").notNull(),
+  creationDate: timestamp("creation_date").defaultNow().notNull(),
+  publicationDate: timestamp("publication_date").defaultNow().notNull(),
+  programmeId: integer("programme_id").references(() => programmesTable.id),
+  promotionId: integer("promotion_id").references(() => promotionsTable.id),
+});
+
+// export const relations = defineRelations ({ programmesTable, promotionsTable, projectsTable}, 
+// (r)=> ({
+//   programmesTable: {
+//     projects: r.many.projectsTable(),
+//   },
+//   promotionsTable:{
+//     projects: r.many.projectsTable(),
+//   },
+//   projectsTable: {
+//     programme: r.one.programmesTable({
+//       from: r.projectsTable.programmeId,
+//       to: r.programmesTable.id,
+//     }),
+//     promotion: r.one.promotionsTable({
+//       from: r.projectsTable.programmeId,
+//       to: r.promotionsTable.id,
+//     })
+//   }
+// }))
