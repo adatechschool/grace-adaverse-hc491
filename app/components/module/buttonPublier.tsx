5f3074830e3d56a78e19b2b91a425157f0a76a8a
Chau <hoaichau20@hotmail.com>
2026-05-06 15:20:36 +0200

    style:page admin

--- app/components/module/buttonPublier.tsx
+++ app/components/module/buttonPublier.tsx
@@ -1,13 +1,19 @@
 'use client'
+
 import { Project } from "@/src/db/types"
 import { publier } from "@/src/db/lib/actions"
 
 interface ProjectProp {
-    project : Project;
+  project: Project;
 }
 
-export default function BouttonPublier({project} : ProjectProp) {
-    return (
-        <button onClick={() => publier(project)}>Publier</button>
-    )
+export default function BouttonPublier({ project }: ProjectProp) {
+  return (
+    <button
+      onClick={() => publier(project)}
+      className="text-xs font-mono text-white bg-blue-400 border border-blue-400 px-4 py-2 rounded-lg hover:bg-blue-500 transition-colors duration-150"
+    >
+      Publier ↗
+    </button>
+  );
 }
