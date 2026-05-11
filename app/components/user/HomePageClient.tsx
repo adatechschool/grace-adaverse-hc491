d4b623689e9d46ae6723d7fadc1e5e5a51907953
Hgo92 <hugo_monier@yahoo.fr>
2026-05-11 09:52:10 +0200

    bug - correction syntaxe import svg

--- app/components/user/HomePageClient.tsx
+++ app/components/user/HomePageClient.tsx
@@ -184,13 +184,13 @@ export default function HomePageClient({
                               xmlns="http://www.w3.org/2000/svg"
                               fill="none"
                               viewBox="0 0 24 24"
-                              stroke-width="1.5"
+                              strokeWidth="1.5"
                               stroke="currentColor"
                               className="size-5"
                             >
                               <path
-                                stroke-linecap="round"
-                                stroke-linejoin="round"
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
                                 d="m4.5 19.5 15-15m0 0H8.25m11.25 0v11.25"
                               />
                             </svg>
